48a6e05062c591ae6513febf853031b7017f5a72
Mehdi Ouaghlani <mouaghlani@octo.com>
2016-08-25 10:33:03 +0200

    Quelques corrections sur le code avant application du pattern

--- decorator_pattern_python/kebab.py
+++ decorator_pattern_python/kebab.py
@@ -7,6 +7,7 @@ PRIX_SALADE = 0.2
 PRIX_TOMATES = 0.2
 PRIX_OIGNONS = 0.3
 PRIX_FRITES = 0.5
+PRIX_FROMAGE = 0.5
 
 Ingredient = namedtuple('Ingredient', 'prix quantite')
 
@@ -18,13 +19,12 @@ class Kebab:
 
         self._ing = dict()
 
-
         self._ing['viande'] = Ingredient(PRIX_MOUTON, 1)
         self._ing['tomates'] = Ingredient(PRIX_TOMATES, 1)
         self._ing['salade'] = Ingredient(PRIX_SALADE, 1)
         self._ing['oignions'] = Ingredient(PRIX_OIGNONS, 1)
         self._ing['frites'] = Ingredient(PRIX_FRITES, 1)
-        self._ing['fromage'] = Ingredient(PRIX_FROMAGE, 1)
+        self._ing['fromage'] = Ingredient(PRIX_FROMAGE, 0)
 
 
     @property
@@ -33,12 +33,12 @@ class Kebab:
 
     def __str__(self):
         s = "Kebab ({0: .2f} sauce {1})".format(self.prix, self.type_sauce)
-        if self.type_viande !="mouton":
+        if self.type_viande != "mouton":
             s += ', au {0}) '.format(self.type_viande)
         for elt in self._ing:
             qte = self._ing[elt].quantite
             if qte > 0:
-                s += ', {0}x{1}'.format(qte, elt)
+                s += ', {0}x {1}'.format(qte, elt)
             else:
                 s += ', sans {0}'.format(elt)
         return s
@@ -50,11 +50,11 @@ class Kebab:
 
     def supp(self, ingredient):
         if ingredient in self._ing:
-            prix, _ =self._ing[ingredient]
+            prix, qte =self._ing[ingredient]
             self._ing[ingredient] = Ingredient(prix, qte+1)
 
     def poulet(self):
-        self.type_viande ='poulet'
+        self.type_viande = 'poulet'
         _, qte = self._ing['viande']
         self._ing['viande'] = Ingredient(PRIX_POULET, qte)
 
